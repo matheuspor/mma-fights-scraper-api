ae55036296fea7f14e62109f7f43e5aabcfacf00
Matheus Pormanhani <matheus.pormanhani@gmail.com>
2022-04-22 15:19:48 +0000

    chore: add nyc

--- services/fight-service.ts
+++ services/fight-service.ts
@@ -2,14 +2,9 @@ import { fetchFights } from '../utils/helper-functions';
 import Fight from '../models/fight-model';
 
 export const populateDatabase = async () => {
-  const fights = await fetchFights();
-    
-  try {
-    await Fight.deleteMany();
-    await Fight.insertMany(fights);
-  } catch (err) {
-    console.log(err);
-  }
+  const fights = await fetchFights();    
+  await Fight.deleteMany();
+  await Fight.insertMany(fights);
 };
   
 export const getAll = async () => Fight.find({}, { _id: 0, _v: 0 });
