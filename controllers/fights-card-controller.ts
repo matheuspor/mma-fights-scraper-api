6fe66b66ae6fc546407495aeb7fe3fcaeba910ba
Matheus <matheus.pormanhani@gmail.com>
2022-04-25 18:09:17 -0300

    feat: add fights-card-controller.ts

---
+++ controllers/fights-card-controller.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from 'express';
+import * as fightsCardService from '../services/fights-card-service';
+
+// eslint-disable-next-line import/prefer-default-export
+export const getAll = async (req: Request, res: Response) => {
+  const fightsCard = await fightsCardService.getAll();
+  return res.status(200).json(fightsCard);    
+};
