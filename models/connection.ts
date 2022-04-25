f818066c78f2a0ec715d1318d6d43a935af0ef55
Matheus <matheus.pormanhani@gmail.com>
2022-04-25 10:14:26 -0300

    fix: testing change

--- models/connection.ts
+++ models/connection.ts
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
 
+console.log(process.env.MONGO_DB_URL);
+
 const connectToDatabase = (
-  mongoDatabaseURI = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/mma-fights',
+  mongoDatabaseURI = process.env.MONGO_DB_URL || 'mongodb://localhost:27017/mma-fights',
 ) => mongoose.connect(mongoDatabaseURI);
 
 export default connectToDatabase;
