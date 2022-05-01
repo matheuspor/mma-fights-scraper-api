da9ad6b38bc6417cba5dc36e3bc7c389d1b30f4b
Matheus Pormanhani <matheus.pormanhani@gmail.com>
2022-05-01 13:31:15 +0000

    refactor: edit fight-controller

--- controllers/fight-controller.ts
+++ controllers/fight-controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import * as fightService from '../services/fight-service';
 
 // eslint-disable-next-line import/prefer-default-export
-export const getAll = async (req: Request, res: Response) => {
+export const getAll = async (_req: Request, res: Response) => {
   const fights = await fightService.getAll();
   return res.status(200).json(fights);    
 };
