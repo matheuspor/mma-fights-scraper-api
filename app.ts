91021b54c819ef28c00073190bffaf6c5b251c1e
Matheus <matheus.pormanhani@gmail.com>
2022-05-04 07:56:02 -0300

    feat: add cors middleware

--- app.ts
+++ app.ts
@@ -2,12 +2,21 @@ import express from 'express';
 import 'dotenv/config';
 import fightsRoutes from './routes/fights-routes';
 import fightsCardRoutes from './routes/fights-card-routes';
+import connectToDatabase from './models/connectToDatabase';
 
 const app = express();
 app.disable('x-powered-by');
 
 app.listen(3000, async () => {
+  connectToDatabase();
   console.log('Server is running on port 3000');
+});
+
+app.use((_req, res, next) => {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'GET');
+  res.set('Access-Control-Allow-Headers', '*');
+  next();
 });
 
 app.use('/api', fightsRoutes);
