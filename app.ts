8357ddcda93205f59d22331f3bc34ae8aa5c2b1d
Matheus <matheus.pormanhani@gmail.com>
2022-05-03 22:21:13 -0300

    chore: deleted check-database middleware

--- app.ts
+++ app.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import 'dotenv/config';
 import fightsRoutes from './routes/fights-routes';
 import fightsCardRoutes from './routes/fights-card-routes';
-import { checkDatabase } from './controllers/middlewares/check-database';
 
 const app = express();
 app.disable('x-powered-by');
@@ -10,8 +9,6 @@ app.disable('x-powered-by');
 app.listen(3000, async () => {
   console.log('Server is running on port 3000');
 });
-
-app.use(checkDatabase);
 
 app.use('/api', fightsRoutes);
 app.use('/api', fightsCardRoutes);
