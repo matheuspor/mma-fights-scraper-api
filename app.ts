a04fbb7648ccd62c49fc8f246fb9816e0e335976
Matheus <matheus.pormanhani@gmail.com>
2022-05-24 14:33:47 -0300

    feat: implements Dockerfile and compose the entire application

--- app.ts
+++ app.ts
@@ -1,19 +1,18 @@
 import express from 'express';
-import dotenv from 'dotenv';
+import 'dotenv/config';
 import fightsRoutes from './routes/fights-routes';
 import fightsCardRoutes from './routes/fights-card-routes';
 import connectToDatabase from './models/connectToDatabase';
 import populateDatabase from './helper-function/populate-database';
 
-dotenv.config();
-
 const app = express();
 app.disable('x-powered-by');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 let deployedDate = new Date('2022-01-01').toLocaleDateString();
 
 app.listen(PORT, async () => {
+  connectToDatabase();
   console.log(`Server is running on port ${PORT}`);
 });
 
@@ -25,7 +24,6 @@ app.use((_req, res, next) => {
 });
 
 app.use(async (_req, _res, next) => {
-  connectToDatabase();
   await populateDatabase(deployedDate);
   deployedDate = new Date().toLocaleDateString();
   next();
