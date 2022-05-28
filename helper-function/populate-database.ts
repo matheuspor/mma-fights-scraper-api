724dc3baaabaf0bc48a34e86e8e378e830a1033c
Matheus <matheus.pormanhani@gmail.com>
2022-05-28 12:00:47 -0300

    refactor: change populateDatabase implementation

--- helper-function/populate-database.ts
+++ helper-function/populate-database.ts
@@ -3,16 +3,22 @@ import * as eventService from '../services/event-service';
 import * as FightsCardService from '../services/fights-card-service';
 
 const populateDatabase = async () => {  
-  await Promise.all([eventService.deleteMany(), FightsCardService.deleteMany()]);
-
+  console.log('populating...');
   const events = await scrapeEvents();
 
-  await Promise.all(
-    [
-      eventService.create(events),
-      scrapeFightsCard(events),
-    ],
-  ).then((values) => FightsCardService.create(values[1]));
+  await Promise.all([eventService.deleteMany(), FightsCardService.deleteMany()])
+    .then(() => {
+      eventService.create(events);
+      return scrapeFightsCard(events);
+    })
+    .then((fights) => FightsCardService.create(fights));
+
+  // await Promise.all(
+  //   [
+  //     eventService.create(events),
+  //     scrapeFightsCard(events),
+  //   ],
+  // ).then((values) => FightsCardService.create(values[1]));
 };
 
 export default populateDatabase;
