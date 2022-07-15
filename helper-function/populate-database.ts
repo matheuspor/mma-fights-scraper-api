6da97867523bec3dba975b42c7773f54c3eb5059
Matheus <matheus.pormanhani@gmail.com>
2022-07-15 11:45:57 -0300

    revert: revert scraper functions

--- helper-function/populate-database.ts
+++ helper-function/populate-database.ts
@@ -1,14 +1,14 @@
-import { scrapeEvents, scrapeFightsCard } from './scraper';
-import * as eventService from '../services/event-service';
-import * as fightsCardService from '../services/fights-card-service';
+import { scrapeEvents, scrapeFightsCard } from './scraper'
+import * as eventService from '../services/event-service'
+import * as fightsCardService from '../services/fights-card-service'
 
-const populateDatabase = async () => {  
-  console.log('populating...');
-  const events = await scrapeEvents();
-  const eventsFights = await scrapeFightsCard(events);
+const populateDatabase = async () => {
+  console.log('populating...')
+  const events = await scrapeEvents()
+  const eventsFights = await scrapeFightsCard(events)
 
   return Promise.all([eventService.deleteMany(), fightsCardService.deleteMany()])
-    .then(() => Promise.all([eventService.create(events), fightsCardService.create(eventsFights)]));
-};
+    .then(() => Promise.all([eventService.create(events), fightsCardService.create(eventsFights)]))
+}
 
-export default populateDatabase;
+export default populateDatabase
